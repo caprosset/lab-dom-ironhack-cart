0b667f708e2ad73aa2494ef1f492fff94fd69e1e
caprosset <capucinerosset@gmail.com>
2019-10-29 17:30:36 +0100

    Complete first iteration

--- js/index.js
+++ js/index.js
@@ -2,7 +2,26 @@ function deleteItem(event){
 
 }
 
+function getTotalPrice() {
+  var productPrice = document.querySelector('.product-price').innerHTML
+  productPrice = Number(productPrice.slice(1))
+  
+  var productQuantity = document.getElementsByTagName('input')[0].value
+
+  var totalPrice = productPrice * productQuantity
+  totalPrice = `$ ${parseFloat(totalPrice).toFixed(2)}`
+
+  var displayTotalPrice =  document.getElementsByClassName('total-price')[0]
+   displayTotalPrice.innerHTML = totalPrice
+
+  return totalPrice //+ totalPrice2
+}
+
+
+
 function getPriceByProduct(itemNode){
+  // var productPrice = document.querySelectorAll('.product-price')
+  // var productQuantity = document.getElementsByTagName('input')[0].value
 
 }
 
@@ -10,9 +29,21 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
 
-}
+
+
+//calculate total price
+var productPrice = document.querySelector('.product-price',).innerHTML
+var productQuantity = document.getElementsByTagName('input')[0].value
+var calculatePriceButton = document.getElementById('calc-prices-button');
+console.log(productQuantity)
+calculatePriceButton.addEventListener('click', getTotalPrice) 
+
+
+
+
+
+
 
 function createQuantityInput(){
 
@@ -39,14 +70,15 @@ function createNewItem(){
 }
 
 window.onload = function(){
+//   // var createItemButton = document.getElementById('new-item-create');
+//   // var deleteButtons = document.getElementsByClassName('btn-delete');
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+//   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].onclick = deleteItem;
+//   }
 };
